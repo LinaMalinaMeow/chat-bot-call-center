d37bae6925ecfd6cd0e37b91a5f674a5ca00f965
Alina Sagaidak <74879251+LinaMalinaMeow@users.noreply.github.com>
2025-01-15 02:23:27 +0300

    chore: update

--- detect_intent.py
+++ detect_intent.py
@@ -7,29 +7,36 @@ from langchain_core.output_parsers import StrOutputParser
 def format_docs(docs):
     return "\n\n".join(f"Sentence: {doc.page_content}\nIntent: {doc.metadata.get('intent', 'Unknown')}" for doc in docs)
 
+
 def detect_intent_with_context(sentence):
-    messages = []
     persist_directory = "db-intents"
     local_embeddings = OllamaEmbeddings(model="llama3.1:8b")
     
     vectorstore = Chroma(persist_directory=persist_directory, embedding_function=local_embeddings)
     
-    docs = vectorstore.similarity_search(sentence)
-    if not docs:
-        messages.append("No relevant intent was found.")
-        return {"response": "No matching intent found.", "messages": messages}
+    docs = vectorstore.similarity_search_with_score(sentence)
+    filtredDocs = []
     
-    print(docs);
+    for doc,score in docs:
+        if score <= 0.25:
+            filtredDocs.append(doc)
+
+    if not filtredDocs:
+        return "Позвоните, пожалуйста, на горячую линию по номеру +79999999999!"
     
-    
+
     INTENT_TEMPLATE = """
-    Ты работник коллцентра в клинике, люди пишут тебе вопросы, ты должен на них отвечать. 
-    Если тебе передается вопрос, похожий на вопрос из базы знаний, отвечай на него,
-    если вопрос вообще не похож на вопросы из базы знаний отвечай: Позвоните, пожалуйста, на горячую линию по номеру [+79999999999](tel:+79999999999)!
+    Ты — человек, консультант клиники, который помогает находить информацию в базе данных и отвечать на вопросы.
+    Вот данные, которые могут содержать полезные сведения по заданному вопросу:
     <context>
     {context}
-    </context>
-    {sentence}"""
+    <context>
+    Вопрос: {sentence}
+    Если вопрос не имеет точную формулировку как в базе знаний, но означает ее, отвечай на него.
+    Если считаешь, что вопроса нет в базе знаний, отвечай: "Позвоните, пожалуйста, на горячую линию по номеру +79999999999!"
+    Ответ должен быть кратким и состоять из информации, соответствующей содержащейся информации.
+    И не пиши, что это ответ на вопрос, пиши просто ответ на вопрос
+    """
     
     intent_prompt = ChatPromptTemplate.from_template(INTENT_TEMPLATE)
     model = ChatOllama(model="llama3.1:8b")
@@ -41,5 +48,5 @@ def detect_intent_with_context(sentence):
         | StrOutputParser()
     )
     
-    response = chain.invoke({"context": docs, "sentence": sentence})
+    response = chain.invoke({"context": filtredDocs, "sentence": sentence})
     return response
